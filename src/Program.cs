bc282b95c0510cd5c371a7f919c51012e9dc645d
vnugent <public@vaughnnugent.com>
2025-08-11 12:06:59 -0400

    refactor: Print actual assembly version during startup

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using Typin;
+﻿using System.Reflection;
+using Typin;
 using Typin.Console;
 
 namespace VNLib.Tools.Build.Executor
@@ -8,12 +9,16 @@ namespace VNLib.Tools.Build.Executor
     {
         static int Main(string[] argsv)
         {
+            //Fetch the current version of the assembly
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string version = assembly.GetName().Version?.ToString() ?? "1.0.0";
+
             return new CliApplicationBuilder()
                  .AddCommandsFromThisAssembly()
                  .UseConsole<SystemConsole>()
                  .UseTitle("VNBuild Copyright (c) Vaughn Nugent")
                  .UseStartupMessage("VNBuild Copyright (c) Vaughn Nugent")
-                 .UseVersionText("0.1.0")
+                 .UseVersionText(version)
                  .Build()
                  .RunAsync()
                  .AsTask()
